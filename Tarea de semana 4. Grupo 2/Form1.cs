d4de5f6d355d576b90a847aca08ec00840e32580
JOSE SAMUEL PARADA QUINTANILLA <USIS016221@ugb.edu.sv>
2021-08-23 11:18:39 -0600

    trabajas

--- Tarea de semana 4. Grupo 2/Form1.cs
+++ Tarea de semana 4. Grupo 2/Form1.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace Tarea_de_semana_4.Grupo_2
 {
-    public partial class Form1 : Form
+    public partial class  Form1 : Form
     {
         // Crea un objeto   aleatorio llamado randomizer
         // para generar números aleatorios.
